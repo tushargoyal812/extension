439a8994631e475895a4a2032abcd0541c1de0a1
tushargoyal812 <69681896+tushargoyal812@users.noreply.github.com>
2022-04-23 23:13:35 +0530

    todo added

--- chrome-extension/src/App.js
+++ chrome-extension/src/App.js
@@ -18,13 +18,35 @@ function App() {
         return {...state,isPassword:false,focus:true,time:action.timeValue}
       case 'greetings':
         return {...state,greetings:action.greet}
+      case 'show-modal':
+        return {...state,showModal:!state.showModal}
       default:
         break;
     }
   }
 
-  const initilaState={userName:"",userEmail:"",showName:true,isEmail:false,isPassword:false,time:null,focus:false,greetings:""}
+  const initilaState={userName:"",userEmail:"",showName:true,isEmail:false,isPassword:false,time:null,focus:false,greetings:"",showModal:false}
+
   const [state,dispatch]=useReducer(reducerFunc,initilaState)
+  const [todoInput,setTodoInput]=useState()
+  const [todoValues,setTodoValues]=useState([])
+  const [editId,setEditId]=useState()
+
+  const addHandler=(value)=>{
+  if(todoValues.some(todo=>todo.id===editId.id)){
+    console.log("if chala",editId);
+    setTodoValues([...todoValues,{...editId,todoName:value}])
+  }else{
+    console.log("else chala");
+    setTodoValues([...todoValues,{id:uuid(),todoName:value,isDone:false}])
+  }
+  setTodoInput("")
+}
+
+  const todoInputHandler=(id)=>{
+  let newValue=todoValues.map(item=>item.id===id?{...item,isDone:!item.isDone}:item)
+  setTodoValues(newValue)
+}
   
   let date=new Date()
   let hours=(date.getHours()<10?"0":"")+date.getHours()
@@ -52,7 +74,21 @@ function App() {
     }
   }
 
-  const {showName,isEmail,userName,isPassword,focus,time,greetings}=state
+  const todoHandler=()=>{
+    dispatch({type:"show-modal"})
+  }
+
+  const cancelHandler=(id)=>{
+    setTodoValues(todoValues.filter(item=>item.id!==id))
+  }
+
+  const editHandler=(item)=>{
+    setTodoInput(item.todoName)
+    setEditId(item)
+    todoValues.some(todos=>todos.id)
+  }
+
+  const {showName,isEmail,userName,isPassword,focus,time,greetings,showModal}=state
 
 
   return (
@@ -63,7 +99,7 @@ function App() {
       <h1 className='main-heading'>Hello,What's your name?</h1>
       <input onChange={(e)=>dispatch({type:"set-name",nameValue:e.target.value})} className='detail-input' type="text" />
       <div>
-      <button onClick={continueNameHandler} className='Countinue-btn'>Continue</button>
+      <button onClick={continueNameHandler} className='Countinue-btn cursor-pointer'>Continue</button>
       </div>
       </div>
       }
@@ -71,7 +107,7 @@ function App() {
       <h1 className='main-heading'>{`What's your email ${userName}`}</h1>
       <input onChange={(e)=>dispatch({type:"set-email",emailValue:e.target.value})} className='detail-input' type="text" />
       <div>
-      <button onClick={continueEmailHandler} className='Countinue-btn'>Continue</button>
+      <button onClick={continueEmailHandler} className='Countinue-btn cursor-pointer'>Continue</button>
       </div>
       </div>
       }
@@ -79,18 +115,37 @@ function App() {
       <h1 className='main-heading'>{`Enter password`}</h1>
       <input className='detail-input' type="password" />
       <div>
-      <button onClick={passwordHandler} className='Countinue-btn'>Continue</button>
+      <button onClick={passwordHandler} className='Countinue-btn cursor-pointer'>Continue</button>
       </div>
       </div>
       }
-      {focus&&<div>
+      {focus&&<div className='main-focus'>
       <div className='main-heading extension-time'>{time}</div>
       <h1 className='main-heading'>{`${greetings} ${userName}`}</h1>
       <h1 className='main-heading'>What's your main focus for today?</h1>
       <input className='detail-input' type="password" />
       <div>
-      <button onClick={passwordHandler} className='Countinue-btn'>Continue</button>
+      <button onClick={passwordHandler} className='Countinue-btn cursor-pointer'>Continue</button>
       </div>
+      {showModal&&<div className='ToDo-modal-container'>
+        <div className='ToDo-modal'>
+          {console.log(todoValues,"from below")}
+          {todoValues.map(item=><div key={item.id}>
+            <div className='todo-container'>
+            <input onChange={()=>todoInputHandler(item.id)} type="checkbox" name="" id="" /><span className='todo-names' style={{textDecoration:item.isDone?"line-through":"none"}}>{item.todoName}</span><span onClick={()=>editHandler(item)} class="material-symbols-rounded">edit</span><span onClick={()=>cancelHandler(item.id)} class="material-symbols-rounded cursor-pointer">close</span>
+            </div>
+          </div>)}
+          <div className='todo-input-container'>
+            <div>
+              <div className='todo-input-button'> 
+          <input value={todoInput} className='todo-input' onChange={(e)=>setTodoInput(e.target.value)} placeholder='New Todo' type="text" autoFocus/>
+          <button className='cursor-pointer todo-add' onClick={()=>addHandler(todoInput)}>Add</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>}
+      <h1 className='ToDo-button cursor-pointer' onClick={todoHandler}>ToDo</h1>
       </div>
       }
     </div>
